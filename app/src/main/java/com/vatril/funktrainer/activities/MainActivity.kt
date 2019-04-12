e003f97c8fbab980d5088fae3a8df16e740b2837
Nikolai Jay Summers <nikolaisummers@gmail.com>
2019-04-12 19:43:49 +0200

    added alphabet screem

--- app/src/main/java/com/vatril/funktrainer/activities/MainActivity.kt
+++ app/src/main/java/com/vatril/funktrainer/activities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.vatril.funktrainer.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.Toast
 import com.vatril.funktrainer.R
 import com.vatril.funktrainer.adapter.RadioListAdapter
 
@@ -12,8 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //findViewById<RecyclerView>(R.id.radiocharlist).adapter =
-        //    RadioListAdapter(this)
-        //findViewById<RecyclerView>(R.id.radiocharlist).layoutManager = LinearLayoutManager(this)
+        val drawer = findViewById<DrawerLayout>(R.id.main_drawer)
+
+
+        findViewById<NavigationView>(R.id.main_nav).setNavigationItemSelectedListener {
+            Toast.makeText(this, it.title, Toast.LENGTH_SHORT).show()
+            drawer.closeDrawers()
+            true
+        }
+
     }
 }
